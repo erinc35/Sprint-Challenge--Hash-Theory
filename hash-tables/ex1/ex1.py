0ab430dad651bd4566d978a0b7c4f7ccaa865eb8
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-10-12 09:18:29 -0700

    hashtables done

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,15 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  dict = {}
+
+  for ind, num in enumerate(weights):
+    if num in dict:
+      return (ind, dict[weights[ind]])
+    else:
+      dict[limit - weights[ind]] = ind
+
+  return ()
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  print(get_indices_of_item_weights([4, 6, 10, 15, 16], 21))
+ 
--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,39 @@
 def reconstruct_trip(tickets):
-  pass 
+  dict = {}
+  length = len(tickets)
+  route = [None] * (length-1)
+  ends = []
+
+  for ticket in tickets:
+    ends.append(ticket[1])
+    if ticket[0] == None:
+      route[0] = ticket[1]
+    dict[ticket[0]] = ticket[1]
+
+  i = 1
+
+  for start in dict:
+    if start not in ends:
+      return []
+
+  while i < length-1:
+    route[i] = dict[route[i-1]]
+    i += 1
+  
+
+  return route  
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass
+  print(reconstructTrip([
+  ['PIT', 'ORD'],
+  ['XNA', 'CID'],
+  ['SFO', 'BHM'],
+  ['FLG', 'XNA'],
+  [None, 'LAX'], 
+  ['LAX', 'SFO'],
+  ['CID', 'SLC'],
+  ['ORD', None],
+  ['SLC', 'PIT'],
+  ['BHM', 'FLG'],
+]))
